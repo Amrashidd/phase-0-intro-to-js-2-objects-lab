ac42feda35f6be1e8866c194b96c51a897b7a81f
ahmed rashid <ahmeddmrashid@gmail.com>
2022-08-07 23:35:58 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name : "Ahmed",
+    streetAddress : "Zaniary",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, name, kutay) {
+    const newEmployee = { ...employee };
+  
+    newEmployee[name] = kutay;
+  
+    return newEmployee;
+  }
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+  employee.streetAddress = "12 Broadway";
+return employee;
+}
+
+ 
+    function deleteFromEmployeeByKey(){
+        const newEmployee = {...employee}
+        delete newEmployee.name;
+        return newEmployee;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    return employee; 
+    }
